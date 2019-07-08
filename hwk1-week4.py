14a1acabf23b96c54f989b10da27a4290051b705
beizaa <51612975+beizaa@users.noreply.github.com>
2019-07-08 22:28:55 +0200

    Add files via upload

---
+++ hwk1-week4.py
@@ -0,0 +1,18 @@
+#ODEV
+#1-)Kullanıcıdan bir input alınız.
+#Input'taki küçük harfleri büyük harfe dönüştüren bir program yazınız.
+#Ör input: Hello World! 
+#Ör output: HELLO WORLD!
+
+girdi=input("En sevdiginiz kelime nedir?") #Kullanicidan bir girdi aliyoruz.
+print(girdi.upper())                       #upper() metodu ile inputtaki kucuk harleri buyuk harfe ceviriyoruz.
+
+#1. Kod calisiyor.
+#2. Bug yok.
+#3. Mantik hatasi yok.
+#4.Degisken isimleri acik.
+#5.Anlasilir bir kod.
+#6.Bilgim dahilinde olmasi gereken uzunlukta.
+#7. Yorum eklenmis.
+#8. Algoritma mantikli. 
+#9.Kapsamli
